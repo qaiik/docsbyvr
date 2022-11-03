bf99afa597f546140103a835574e244d97123c59
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-11-03 13:00:34 -0400

    slight fixes

--- snippets/node.js/remoteSocket.js
+++ snippets/node.js/remoteSocket.js
@@ -1,11 +1,13 @@
-const io = require("socket.io-client")l
+const io = require("socket.io-client");
 
 let socket = io.connect("https://betastar.org/", {
   transportOptions: {
     polling: {
       extraHeaders: {
-        Cookie: "your betastar cookie"
+        Cookie: "Account PHPSESSID"
       }
     }
   }
 })
+
+// You can now use socket.on() and socket.emit()
